6cdb7d7a054f28649da0429b32ce932017d09bc7
zenith <atupuertadelivery.admon@gmail.com>
2026-07-04 16:59:58 -0400

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.ONDELIVERY_SUPABASE_CONFIG = {
+  url: 'https://knnnjyeinpbrurrcctya.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtubm5qeWVpbnBicnVycmNjdHlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxODc2NTYsImV4cCI6MjA5ODc2MzY1Nn0.0J9XDcFSqgNTSr4Tg8T6s_qJAAJdeW3kr5vPvWUTYZ4'
+};
